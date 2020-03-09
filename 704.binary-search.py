8b54121d8ef2b4097d7764a95a28c72738f1b76a
Yuqing Lu <yuqinglu@YuqingdeMacBook-Pro.local>
2020-03-08 17:48:18 -0700

    Binary Search 704 pass

--- 704.binary-search.py
+++ 704.binary-search.py
@@ -5,26 +5,6 @@
 #
 
 # @lc code=start
-def binary_search(nums, target):
-    if len(nums) == 0 or nums is None:
-        return -1
-    left = 0
-    right = len(nums)-1
-    while left <= right:
-        mid = (left + right)/2
-        mid = int(float(mid))
-        if target < nums[mid]:
-            right = mid - 1
-        elif target > nums[mid]:
-            left = mid + 1
-        else:
-            return mid
-    return None
-
-result = binary_search([1,2,3,4,6], 4)
-print(result)
-
-
 class Solution(object):
     def search(self, nums, target):
         """
@@ -32,7 +12,19 @@ class Solution(object):
         :type target: int
         :rtype: int
         """
-        
-        
+        if len(nums) == 0 or nums is None:
+            return -1
+        left = 0
+        right = len(nums)-1
+        while left <= right:
+            mid = (left + right)/2
+            mid = int(float(mid))
+            if target < nums[mid]:
+                right = mid - 1
+            elif target > nums[mid]:
+                left = mid + 1
+            else:
+                return mid
+        return -1  
 # @lc code=end
 
